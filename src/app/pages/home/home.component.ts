f5259a8713df121c572fa22e10770d956ce15b73
Pepe Puertas Alonso <pepepuertasalonso4@gmail.com>
2025-03-09 13:26:45 +0100

    Creación de home y cards de usuarios

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { IUser } from '../../interfaces/iuser.interface';
+import { UserService } from '../../services/user.service';
+import { UserCardComponent } from '../../components/user-card/user-card.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [UserCardComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  arrUsers: IUser[] = [];
+  userService = inject(UserService);
+  currentPage: number = 1;
+  maxPage!: number;
 
+  ngOnInit() {
+    this.loadUser();
+  }
+
+  async loadUser(page: number = 1) {
+    try {
+      this.currentPage = page <= 0 ? 1 : page;
+      let response = await this.userService.getAll(this.currentPage);
+      this.maxPage = response.total_pages;
+      this.arrUsers = response.results;
+      console.log(this.maxPage);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
