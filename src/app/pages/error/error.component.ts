bee01b37b3f5f9c7a0a239d5b53a84e7fcc29a42
Pepe Puertas Alonso <pepepuertasalonso4@gmail.com>
2025-03-09 16:27:32 +0100

    Refactorizando y eliminando código innecesario

--- src/app/pages/error/error.component.ts
+++ src/app/pages/error/error.component.ts
@@ -10,13 +10,4 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 export class ErrorComponent {
   errorCode: number = 404;
   errorMessage: string = 'Lo sentimos, la página que buscas no existe.';
-
-  constructor(private route: ActivatedRoute) {}
-
-  ngOnInit() {
-    this.route.data.subscribe((data) => {
-      this.errorCode = data['errorCode'] || this.errorCode;
-      this.errorMessage = data['errorMessage'] || this.errorMessage;
-    });
-  }
 }
